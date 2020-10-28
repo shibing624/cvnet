315492ed1e5b1304a28d5d98c2dbd7e5f90fc517
xuming624 <xuming624@qq.com>
2020-10-28 22:47:12 +0800

    add style transfer.

--- examples/yolo_detection.py
+++ examples/yolo_detection.py
@@ -4,17 +4,4 @@
 @description: 
 """
 
-from imageai.Detection import ObjectDetection
-
-detector = ObjectDetection()
-detector.setModelTypeAsTinyYOLOv3()
-detector.setModelPath('../data/yolo-tiny.h5')
-detector.loadModel()
-
-detections, objects_path = detector.detectObjectsFromImage('../data/lap.png', output_image_path="lap_yolo_tiny.jpg",
-                                                           extract_detected_objects=True)
-
-for i, each_path in zip(detections, objects_path):
-    print(i['name'] + ':' + str(i['percentage_probability']))
-    print("Object's image saved in " + each_path)
-    print("--------------------------------")
+print(1e3)
