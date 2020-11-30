6381cf38a1ec6bb3af9cc3d10f3f2e8192315697
xuming624 <xuming624@qq.com>
2020-11-30 16:11:02 +0800

    update fig

--- cvnet/figure.py
+++ cvnet/figure.py
@@ -1,16 +1,18 @@
 """The image module contains functions for plotting"""
+import numpy as np
 from IPython import display
 from matplotlib import pyplot as plt
-import numpy as np
 
 __all__ = ['plt', 'bbox_to_rect', 'semilogy', 'set_figsize', 'show_bboxes',
            'show_images', 'show_trace_2d', 'use_svg_display', 'plot', 'set_axes', 'Animator']
 
+
 def bbox_to_rect(bbox, color):
     """Convert bounding box to matplotlib format."""
-    return plt.Rectangle(xy=(bbox[0], bbox[1]), width=bbox[2]-bbox[0],
-                         height=bbox[3]-bbox[1], fill=False, edgecolor=color,
+    return plt.Rectangle(xy=(bbox[0], bbox[1]), width=bbox[2] - bbox[0],
+                         height=bbox[3] - bbox[1], fill=False, edgecolor=color,
                          linewidth=2)
+
 
 def semilogy(x_vals, y_vals, x_label, y_label, x2_vals=None, y2_vals=None,
              legend=None, figsize=(3.5, 2.5)):
@@ -30,12 +32,14 @@ def set_figsize(figsize=(3.5, 2.5)):
     use_svg_display()
     plt.rcParams['figure.figsize'] = figsize
 
+
 def _make_list(obj, default_values=None):
     if obj is None:
         obj = default_values
     elif not isinstance(obj, (list, tuple)):
         obj = [obj]
     return obj
+
 
 def show_bboxes(axes, bboxes, labels=None, colors=None):
     """Show bounding boxes."""
@@ -51,6 +55,7 @@ def show_bboxes(axes, bboxes, labels=None, colors=None):
                       va='center', ha='center', fontsize=9, color=text_color,
                       bbox=dict(facecolor=color, lw=0))
 
+
 def show_images(imgs, num_rows, num_cols, scale=2):
     """Plot a list of images."""
     figsize = (num_cols * scale, num_rows * scale)
@@ -61,6 +66,7 @@ def show_images(imgs, num_rows, num_cols, scale=2):
             axes[i][j].axes.get_xaxis().set_visible(False)
             axes[i][j].axes.get_yaxis().set_visible(False)
     return axes
+
 
 def show_trace_2d(f, res):
     """Show the trace of 2D variables during optimization."""
@@ -74,9 +80,11 @@ def show_trace_2d(f, res):
     plt.xlabel('x1')
     plt.ylabel('x2')
 
+
 def use_svg_display():
     """Use svg format to display plot in jupyter."""
     display.set_matplotlib_formats('svg')
+
 
 def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None,
          ylim=None, xscale='linear', yscale='linear', fmts=None,
@@ -84,22 +92,23 @@ def plot(X, Y=None, xlabel=None, ylabel=None, legend=[], xlim=None,
     """Plot multiple lines"""
     set_figsize(figsize)
     axes = axes if axes else plt.gca()
-    #if isinstance(X, nd.NDArray): X = X.asnumpy()
-    #if isinstance(Y, nd.NDArray): Y = Y.asnumpy()
+    # if isinstance(X, nd.NDArray): X = X.asnumpy()
+    # if isinstance(Y, nd.NDArray): Y = Y.asnumpy()
     if not hasattr(X[0], "__len__"): X = [X]
-    if Y is None: X, Y = [[]]*len(X), X
+    if Y is None: X, Y = [[]] * len(X), X
     if not hasattr(Y[0], "__len__"): Y = [Y]
     if len(X) != len(Y): X = X * len(Y)
-    if not fmts: fmts = ['-']*len(X)
+    if not fmts: fmts = ['-'] * len(X)
     axes.cla()
     for x, y, fmt in zip(X, Y, fmts):
-        #if isinstance(x, nd.NDArray): x = x.asnumpy()
-        #if isinstance(y, nd.NDArray): y = y.asnumpy()
+        # if isinstance(x, nd.NDArray): x = x.asnumpy()
+        # if isinstance(y, nd.NDArray): y = y.asnumpy()
         if len(x):
             axes.plot(x, y, fmt)
         else:
             axes.plot(y, fmt)
     set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
 
 def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
     """A utility function to set matplotlib axes"""
@@ -120,9 +129,9 @@ class Animator(object):
         """Incrementally plot multiple lines."""
         use_svg_display()
         self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
-        if nrows * ncols == 1: self.axes = [self.axes,]
+        if nrows * ncols == 1: self.axes = [self.axes, ]
         # use a lambda to capture arguments
-        self.config_axes = lambda : set_axes(
+        self.config_axes = lambda: set_axes(
             self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
         self.X, self.Y, self.fmts = None, None, fmts
 
